231fd2cbcc4f7f8bdf6717aabdafa3c80f7e0a5d
MartyRath <martydavidger@gmail.com>
2024-04-19 15:28:32 +0100

    HTTPS lab working

--- 03-load-balancer.tf
+++ 03-load-balancer.tf
@@ -2,7 +2,7 @@
 # Contents:
 # 1. Target Group
 # 2. Application Load Balancer
-# 3. Listener
+# 3. Listeners
 
 ########################## 1. Target Group ######################################################
 # Creating an instance target group to be used with application load balancer
@@ -24,7 +24,7 @@ resource "aws_lb" "application_lb" {
   security_groups    = [aws_security_group.web_server_sg.id]
 }
 
-########################## 3. Listener ######################################################
+########################## 3. Listeners ######################################################
 # Listener for HTTP traffic
 resource "aws_lb_listener" "http_listener" {
   load_balancer_arn = aws_lb.application_lb.arn
@@ -37,3 +37,18 @@ resource "aws_lb_listener" "http_listener" {
     target_group_arn = aws_lb_target_group.web_server_tg.arn
   }
 }
+
+# Listener for HTTPS traffic. Must be set up with Load Balancer DNS name, then insert cert ARN below.
+#resource "aws_lb_listener" "https_listener" {
+#  load_balancer_arn = aws_lb.application_lb.arn
+#  port              = "443"
+#  protocol          = "HTTPS"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#  certificate_arn   = "INSERT_CERT_ARN_HERE"
+
+# Forward to target group
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = aws_lb_target_group.web_server_tg.arn
+#  }
+#}
