db5233a9feb35e02e2bfab41cec64934686651c6
MartyRath <martydavidger@gmail.com>
2024-04-22 09:31:17 +0100

    Custom metrics running every 1 minute. Added error handling to custom metrics.

--- variables.tf
+++ variables.tf
@@ -8,6 +8,9 @@ variable "web_server_script" {
       # Update OS
       yum update -y
 
+      # Installing cron
+      yum install cronie cronie-anacron -y
+
       # Install Apache web server
       yum install httpd -y
 
@@ -34,8 +37,6 @@ variable "template_script" {
   default = <<-EOF
     #!/bin/bash
 
-    yum install cronie cronie-anacron -y
-
     # Create IMDSv2 session token
     TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
     INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
@@ -47,20 +48,30 @@ variable "template_script" {
 
     push_custom_metrics() {
       echo "Running custom metrics"
+      
+      # Ensure region is set to us-east-1
+      aws configure set region us-east-1
+      
       INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
       USEDMEMORY=$(free -m | awk 'NR==2{printf "%.2f\t", $3*100/$2 }')
       TCP_CONN=$(netstat -an | wc -l)
       TCP_CONN_PORT_80=$(netstat -an | grep 80 | wc -l)
       IO_WAIT=$(iostat | awk 'NR==4 {print $5}')
 
-      aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY
-      aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN
-      aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_80
-      aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT
+      # Added error handling for AWS CLI commands
+      if ! aws cloudwatch put-metric-data --metric-name memory-usage --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $USEDMEMORY; then
+          echo "Error pushing memory usage metric to CloudWatch"
+      fi
+      if ! aws cloudwatch put-metric-data --metric-name Tcp_connections --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN; then
+          echo "Error pushing TCP connections metric to CloudWatch"
+      fi
+      if ! aws cloudwatch put-metric-data --metric-name TCP_connection_on_port_80 --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $TCP_CONN_PORT_80; then
+          echo "Error pushing TCP connections on port 80 metric to CloudWatch"
+      fi
+      if ! aws cloudwatch put-metric-data --metric-name IO_WAIT --dimensions Instance=$INSTANCE_ID --namespace "Custom" --value $IO_WAIT; then
+          echo "Error pushing IO wait metric to CloudWatch"
+      fi
     }
-
-    # Ensure region is set to us-east-1
-    aws configure set region us-east-1
 
     while true; do
       push_custom_metrics
