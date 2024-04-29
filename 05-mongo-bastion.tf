8b48a95e254fe00c2f355b6c077fb090ca0826a8
MartyRath <martydavidger@gmail.com>
2024-04-29 10:21:56 +0100

    Updated mongo-bastion file

--- 05-mongo-bastion.tf
+++ 05-mongo-bastion.tf
@@ -1,29 +1,27 @@
+# Contents:
+# 1. Mongo instance
+# 2. Bastion instance
 
-# Creates a Mongo Instance in a private subnet 
-
+# 1. Mongo Instance in a private subnet 
 resource "aws_instance" "mongo_instance" {
   ami           = data.aws_ami.most_recent_amazon_ami.id
   instance_type = "t2.nano"
   subnet_id     = module.vpc.private_subnets[0] # Creates instance in private VPC subnet
-  # Currently allows ssh from anywhere
   vpc_security_group_ids = [aws_security_group.mongo_sg.id]
-  iam_instance_profile   = "LabInstanceProfile" # To be used to push custom metrics to CloudWatch
-
-  key_name = "firstLabKey"
+  key_name = "firstLabKey" # Allows SSH from Bastion
 
   tags = {
     Name = "Mongo Database"
   }
 }
 
+# 2. Bastion instance to connect to the Mongo Instance
 resource "aws_instance" "bastion_instance" {
   ami           = data.aws_ami.most_recent_amazon_ami.id
   instance_type = "t2.nano"
   subnet_id     = module.vpc.public_subnets[0] # Creates instance in public VPC subnet
-  # Currently allows ssh from anywhere
   vpc_security_group_ids = [aws_security_group.bastion_sg.id] # Uses bastion security group
-
-  key_name = "firstLabKey"
+  key_name = "firstLabKey" # Currently allows ssh from anywhere
 
   tags = {
     Name = "Bastion Instance"
