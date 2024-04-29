4a0ad8a39d80707f8dc66bf1449b11d23811bebd
MartyRath <martydavidger@gmail.com>
2024-04-29 10:32:53 +0100

    Updated outputs

--- output.tf
+++ output.tf
@@ -1,11 +1,12 @@
 # Use to output data such as ec2 public ip or id
 
 output "most_recent_amazon_ami" {
+  description = "Most recent Amazon Linux x86_64 AMI"
   value = data.aws_ami.most_recent_amazon_ami.id
 }
 
 output "instance_public_ip" {
-  description = "Public IP of EC2 Instance"
+  description = "Public IP of Main web server instance"
   value       = aws_instance.main_web_server.public_ip
 }
 
